b109dd626d09331294d2541387b23690c7e145a5
January J. <galore.module5h@icloud.com>
2025-10-29 12:09:23 +0400

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,13 +1,12 @@
-// models/Transaction.js
-const mongoose = require('mongoose');
-const TransactionSchema = new mongoose.Schema({
-  client: { type: mongoose.Schema.Types.ObjectId, ref: 'Client', index: true },
-  date:   { type: Date, default: Date.now },
-  currency: { type: String, required: true },
-  amount:   { type: Number, required: true }, // +credit / -debit
-  type:     { type: String, enum: ['credit','debit'], required: true },
-  description: String,
-  balanceAfter: Number
+const { Schema, model, Types } = require('mongoose');
+
+const TxSchema = new Schema({
+  client: { type: Types.ObjectId, ref: 'Client', index: true },
+  type: { type: String, enum: ['credit', 'debit'], required: true },
+  amount: { type: Number, required: true },
+  currency: { type: String, default: 'USD' },
+  memo: String,
+  date: { type: Date, default: Date.now, index: true }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Transaction', TransactionSchema);
+module.exports = model('Transaction', TxSchema);
